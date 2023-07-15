44e0a030b35c42b77931a5718d00247f9a0e661f
BerlisIlya <iberlis@yandex.ru>
2023-07-15 17:48:52 +0300

    update

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -13,11 +13,11 @@ public class CarService {
     public CarService() {
 
         carList = new ArrayList<>();
-        carList.add(new Car("Volvo", "black", 2022, 190));
+        carList.add(new Car("Volvo", "black", 2022, 250));
         carList.add(new Car("Toyota", "white", 2002, 190));
-        carList.add(new Car("Lada", "green", 2016, 190));
-        carList.add(new Car("Skoda", "brown", 1999, 190));
-        carList.add(new Car("Nissan", "black", 2009, 190));
+        carList.add(new Car("Lada", "green", 2016, 90));
+        carList.add(new Car("Skoda", "brown", 1999, 140));
+        carList.add(new Car("Nissan", "black", 2009, 320));
     }
 
     public List<Car> getAllCars() {
